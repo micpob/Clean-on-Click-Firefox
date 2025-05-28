aacfa160823ac458a592581f5b15290f045566f1
micpob <bomboloapp@gmail.com>
2025-05-28 08:45:10 +0200

    Port Chrome code to Firefox

--- background.js
+++ background.js
@@ -16,7 +16,7 @@ chrome.runtime.onInstalled.addListener((details) => {
         chrome.storage.local.set({
           'active': true,
         }, () => {
-          setUpContextMenus()
+          //setUpContextMenus()
         })
         break;
      case 'update':
@@ -25,9 +25,9 @@ chrome.runtime.onInstalled.addListener((details) => {
         chrome.storage.local.set({
           "active": active
         }, () => {
-          chrome.contextMenus.removeAll(() => {
+          /* chrome.contextMenus.removeAll(() => {
             setUpContextMenus()
-          })
+          }) */
         })
       })
         break;
@@ -47,3 +47,20 @@ chrome.action.onClicked.addListener( () => {
     }
   })  
 })
+
+/* const setUpContextMenus = () => {
+  chrome.contextMenus.removeAll(() => {
+    const contextMenuMyOtherExtyensions = {
+      id: 'myOtherExtensions',
+      title: chrome.i18n.getMessage("context_menu_my_other_extensions"),
+      contexts: ['action']
+    }
+    chrome.contextMenus.create(contextMenuMyOtherExtyensions, () => chrome.runtime.lastError)
+  })
+}
+
+chrome.contextMenus.onClicked.addListener((clickData) => {
+  if (clickData.menuItemId == 'myOtherExtensions') {
+    chrome.tabs.create({ url: 'https://chromewebstore.google.com/search/micpob' })
+  }
+}) */
